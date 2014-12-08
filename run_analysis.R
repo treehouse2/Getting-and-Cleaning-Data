eae71bfbc66ed7c3912ef6a0a234fb7571b44ffb
treehouse2 <treehouse2@users.noreply.github.com>
2014-12-07 23:28:24 -0500

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,61 @@
+## 1.Merges the training and the test sets to create one data set.
+
+setwd("~/Data Science/UCI HAR Dataset")
+
+train_data <- read.table("./train/X_train.txt")
+train_label <- read.table("./train/y_train.txt")
+train_subject <- read.table("./train/subject_train.txt")
+
+test_data <- read.table("./test/X_test.txt")
+test_label <- read.table("./test/y_test.txt")
+test_subject <- read.table("./test/subject_test.txt")
+
+myData <- rbind(train_data, test_data)
+mylabel <- rbind(train_label, test_label)
+mysubject <- rbind(train_subject, test_subject)
+
+
+## 2.Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+features <- read.table("features.txt")
+mean_std_indexes <- grep("-mean\\(\\)|-std\\(\\)", features[, 2])
+myData <- myData[,mean_std_indexes]
+
+## 3.Uses descriptive activity names to name the activities in the data set
+
+names(myData) <- gsub("\\(\\)", "", features[mean_std_indexes, 2])
+
+## 4.Appropriately labels the data set with descriptive variable names. 
+
+activity <- read.table("activity_labels.txt")
+activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
+mylabel[, 1] <- activity[mylabel[, 1], 2]
+names(mylabel) <- "activity"
+
+names(mysubject) <- "subject"
+cleaned <- cbind(mysubject, mylabel, myData)
+write.table(cleaned, "merged_clean_data.txt")
+
+## 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+subjectLen <- length(unique(mysubject)[,1])
+activityLen <- length(activity[,1])
+column_Len = dim(cleaned)[2]
+result = cleaned[1:(subjectLen*activityLen), ]
+
+row <- 1
+for(i in 1:subjectLen) {
+  for(j in 1:activityLen) {
+    result[row, 1] <- sort(unique(mysubject)[, 1])[i]
+    result[row, 2] <- activity[j, 2]
+    numCols <- dim(cleaned)[2]
+    
+    myresult <- cleaned[cleaned$subject==i & cleaned$activity==activity[j, 2], ]
+    result[row, 3:numCols] <- colMeans(myresult[, 3:numCols])
+    
+    row <- row + 1
+  }
+}
+
+head(result)
+write.table(result, "clean_data_with_means.txt")
