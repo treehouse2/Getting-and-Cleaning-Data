f14764cd2d86e1189a7b3b18c988f2f30b648976
treehouse2 <treehouse2@users.noreply.github.com>
2014-12-20 01:14:01 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -34,7 +34,6 @@ names(mylabel) <- "activity"
 
 names(mysubject) <- "subject"
 cleaned <- cbind(mysubject, mylabel, myData)
-write.table(cleaned, "merged_clean_data.txt", sep="\t")
 
 ## 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
